8e00c6ed6cb0903887294cbb40b307eac08f13e7
Sina Klampt <sklampt@student.ethz.ch>
2021-05-25 13:31:10 +0200

    code ex1.2

--- Project6/pde-miniapp/data.cpp
+++ Project6/pde-miniapp/data.cpp
@@ -32,21 +32,26 @@ void SubDomain::init(int mpi_rank, int mpi_size, Discretization& discretization)
 {
     int dims[2] = { 0, 0 };
     // TODO determine the number of subdomains in the x and y dimensions using "MPI_Dims_create"  
+    MPI_Dims_create(mpi_size, 2, dims);
  
     ndomy = dims[0];
     ndomx = dims[1];
 
     int periods[2] = { 0, 0 };
     // TODO create a 2D non-periodic cartesian topology using "MPI_Cart_create"
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &comm_cart);
 
     int coords[2];
     // TODO retrieve coordinates of the rank in the topology using "MPI_Cart_coords"
+    MPI_Cart_coords(comm_cart, mpi_rank, 2, coords);
 
     domy = coords[0]+1;
     domx = coords[1]+1;
 
     // TODO set neighbours for all directions using "MPI_Cart_shift" 
     // i.e. set neighbour_south neighbour_north neighbour_east neighbour_west
+    MPI_Cart_shift(comm_cart, 0, -1, &neighbour_north, &neighbour_south);
+    MPI_Cart_shift(comm_cart, 1, 1, &neighbour_west, &neighbour_east);
 
     // get bounding box
     nx = discretization.nx / ndomx;
